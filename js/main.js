f0cac34e1e1d29dd3e77c444452d630a1fce9072
Taylor Chin <taylorychin@gmail.com>
2021-06-07 11:46:30 -0700

    adding checkHand and hit functions. Stubbed init and render functions, updated code to build master deck.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 /*
     constant to hold master copy of deck.
 */
-var suits = ["spades", "diamonds", "clubs", "hearts"];
-var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-const deckMaster = [];
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const masterDeck = buildMasterDeck();
 
 
 /*----- app's state (variables) -----*/
@@ -15,6 +15,8 @@ first move ( blocks double down, )
 var deck = [];
 var dealerHand = [];
 var playerHand = [];
+var funds;
+var bet;
 
 
 
@@ -30,17 +32,64 @@ var playerHand = [];
 //button click listeners for deal, chips, hold, hit, x2down, etc.
 
 /*----- functions -----*/
+function init() {
 
-//create a deck of cards. Make the master deck. Ensures the deck is empty before pushing cards.
-function initDeck() {
-    deckMaster.length = 0;
-    for (i = 0; i < suits.length; i++) {
-        for (j = 0; j < values.length; j++) {
-            var card = { suit: suits[i], value: values[j] };
-            deckMaster.push(card);
+}
+
+function render() {
+
+}
+
+function bet(amount) {
+
+}
+
+function buildMasterDeck() {
+    const deck = [];
+    // Use nested forEach to generate card objects
+    suits.forEach(function (suit) {
+        ranks.forEach(function (rank) {
+            deck.push({
+                // The 'face' property maps to the library's CSS classes for cards
+                face: `${suit}${rank}`,
+                // Setting the 'value' property for game of blackjack, not war
+                value: Number(rank) || (rank === 'A' ? 11 : 10)
+            });
+        });
+    });
+    return deck;
+}
+
+//check value of hand. return value of hand? or 
+function checkHand(hand) {
+    let aces = [];
+    score = 0;
+    hand.forEach(function (card,) {
+        if (card.rank === 'A') aces++;
+        score += card.value;
+    });
+    //reduce aces if over 21
+    if ((score > 21) && (aces > 0)) {
+        while ((aces > 0) && (score > 21)) {
+            score -= 10;
+            aces--;
         }
     }
+    return score;
 }
+
+//pass in hand 
+function hit(hand) {
+    let card = deck.pop();
+    hand.push(card);
+}
+
+//function needed for computing stay?
+function stay(hand) {
+
+}
+
+
 
 //shuffle the passed in deck array; Do this 10 times.
 function shuffle(deck) {
@@ -55,5 +104,3 @@ function shuffle(deck) {
 
 }
 
-console.log(values.length);
-console.log(suits.length);
