9114996a256f2d3b0be9220ccfc75b151d64fee9
Taylor Chin <taylorychin@gmail.com>
2021-06-07 23:39:07 -0700

    beginning of event listener

--- js/main.js
+++ js/main.js
@@ -12,38 +12,76 @@ const masterDeck = buildMasterDeck();
 best stage : blocks betting once deal is clicked.
 first move ( blocks double down, )
 */
-var deck = [];
-var dealerHand = [];
-var playerHand = [];
+gameStage; //modulus operater this to decide where what step the game is on?
 var funds;
 var bet;
+
+var testVar1;
+var TestVar2;
+var testVar3;
 
 
 
 
 /*----- cached element references -----*/
+var modifier; // changes how much you win.
+var deck = [];
+var dealerHand = [];
+var playerHand = [];
+var totalMoney;
+
 //player's hand
-//dealer's hand.
+//dealer's hand
 //bet total
 //money
 
 
 /*----- event listeners -----*/
 //button click listeners for deal, chips, hold, hit, x2down, etc.
+document.querySelector('button').addEventListener('click', handleButtons);
 
 /*----- functions -----*/
+//setup or reset the game.
 function init() {
 
 }
-
+//=============================================
 function render() {
 
 }
 
+//=============================================
 function bet(amount) {
 
 }
 
+//=============================================
+function didWin() {
+    dScore = checkHand(dealerHand);
+    pScore = checkHand(playerHand);
+
+    if ((pScore > dScore) && (pScore < 22)) return true;
+    else if (dScore > 21) return true;
+    else return false;
+
+}
+
+//===============================================
+function renderDeckInContainer(deck, container) {
+    container.innerHTML = '';
+    // Let's build the cards as a string of HTML
+    let cardsHtml = '';
+    deck.forEach(function (card) {
+        cardsHtml += `<div class="card ${card.face}"></div>`;
+    });
+    // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
+    // const cardsHtml = deck.reduce(function(html, card) {
+    //   return html + `<div class="card ${card.face}"></div>`;
+    // }, '');
+    container.innerHTML = cardsHtml;
+}
+
+//======================================================
 function buildMasterDeck() {
     const deck = [];
     // Use nested forEach to generate card objects
@@ -59,7 +97,7 @@ function buildMasterDeck() {
     });
     return deck;
 }
-
+//========================================================
 //check value of hand. return value of hand? or 
 function checkHand(hand) {
     let aces = [];
@@ -77,20 +115,14 @@ function checkHand(hand) {
     }
     return score;
 }
-
+//=================================================
 //pass in hand 
 function hit(hand) {
     let card = deck.pop();
     hand.push(card);
 }
 
-//function needed for computing stay?
-function stay(hand) {
-
-}
-
-
-
+//==================================================
 //shuffle the passed in deck array; Do this 10 times.
 function shuffle(deck) {
     for (x = 0; x < 10; x++) {
